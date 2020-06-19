018191f99f72491243c5e035beb9f6667eb28d0f
Ivan Hornung <ivanhor2003@gmail.com>
2020-06-18 17:19:12 -0700

    Create python server demo and updated clientdemo

--- Interfacing with the Raspberry Pi/Module 2 - Socket Interface/PythonClientDemo.sh
+++ Interfacing with the Raspberry Pi/Module 2 - Socket Interface/PythonClientDemo.sh
@@ -6,7 +6,7 @@ pi@raspberrypi:~ $ python3
 
 >>> import socket
 >>> ms = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
->>> ms.connect(("127.0.0.1", 1234))
+>>> ms.connect(("127.0.0.1", 1234)) #(IP, port#); IP = loopback address (sending to itself)
 >>> ms.sendall(b"Hello world!")
 >>> ms.sendall(b"Hello again!")
 
