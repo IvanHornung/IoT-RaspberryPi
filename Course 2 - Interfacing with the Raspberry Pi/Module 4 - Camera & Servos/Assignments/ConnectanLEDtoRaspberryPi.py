53c43d038ac6d714f5ca63f2e6b96742d02910cb
Ivan Hornung <ivanhor2003@gmail.com>
2020-06-20 00:48:20 -0700

    Finished 'Interfacing with the Raspberry Pi' Course

--- Course 2 - Interfacing with the Raspberry Pi/Module 4 - Camera & Servos/Assignments/ConnectanLEDtoRaspberryPi.py
+++ Course 2 - Interfacing with the Raspberry Pi/Module 4 - Camera & Servos/Assignments/ConnectanLEDtoRaspberryPi.py
@@ -13,13 +13,13 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
-pwn = GPIO.PWN(12, 50)
-pwn.start(0)
+pwm = GPIO.PWM(12, 50)
+pwm.start(0)
 
 while True:
     for i in range(100):
-        pwn.ChangeDutyCycle(i)
+        pwm.ChangeDutyCycle(i)
         time.sleep(0.1)
     for i in range(100, 0, -1):
-        pwn.ChangeDutyCycle(i)
+        pwm.ChangeDutyCycle(i)
         time.sleep(0.1)
